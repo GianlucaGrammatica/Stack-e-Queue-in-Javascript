7e7366261d45a1de24b82fc65b3cba3a8e0df6d6
Gianluca <10934204@itisrossi.vi.it>
2024-11-29 11:14:38 +0100

    Basic css

--- script.js
+++ script.js
@@ -23,6 +23,24 @@ class Stack {
         return this.BaseArray[this.BaseArray.length];
     }
 
+    ToString(){
+        let toReturn = "";
+        for(let i = 0; i < this.Count; i++){
+            toReturn += `${this.BaseArray[i]} `
+        }
+
+        return toReturn;
+    }
+
+    ToHTML(){
+        let toReturn = ``;
+        for(let i = 0; i < this.Count; i++){
+            toReturn += `<h4 id="StackVisualizer" class="Element">${this.BaseArray[i]}</h4>`
+        }
+
+        return toReturn;
+    }
+
     Clear(){
         this.BaseArray = [];
         this.Count = 0;
@@ -60,3 +78,21 @@ class Queue {
         this.Count = 0;
     }
 }
+
+let StackVisualizer = document.getElementById("StackVisualizer");
+let InputAddButton = document.getElementById("InputAddButton");
+let TextInput = document.getElementById("TextInput");
+
+let shtack = new Stack();
+
+
+InputAddButton.addEventListener("click", StackPush);
+
+function StackPush(){
+    if(TextInput.value != ""){
+        shtack.Push(TextInput.value);
+        StackVisualizer.innerHTML = shtack.ToHTML();
+        TextInput.value = "";
+    }
+}
+
