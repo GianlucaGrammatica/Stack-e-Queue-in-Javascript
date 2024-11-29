5b31f797e60dc53459e6bee16e5f98b76148318d
Gianluca <10934204@itisrossi.vi.it>
2024-11-29 10:30:46 +0100

    Fatte le classi di base

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
 console.log("Abcia2 :D");
+
+class Stack {
+    BaseArray;
+    Count = 0;
+
+    constructor(){
+        this.BaseArray = [];
+        this.Count = 0;
+    }
+
+    Push(value){
+        this.BaseArray.push(value);
+        this.Count++;
+    }
+
+    Pop(){
+        this.Count--;
+        return this.BaseArray.pop();
+    }
+
+    Peak(){
+        return this.BaseArray[this.BaseArray.length];
+    }
+
+    Clear(){
+        this.BaseArray = [];
+        this.Count = 0;
+    }
+}
+
+class Queue {
+    BaseArray;
+    Count = 0;
+
+    constructor(){
+        this.BaseArray = [];
+        this.Count = 0;
+    }
+
+    Enque(value){
+        this.BaseArray.push(value);
+        this.Count++;
+    }
+
+    Deque(){
+        this.Count--;
+        let toReturn = this.BaseArray[0];
+        this.BaseArray = this.BaseArray.slice(1);
+
+        return toReturn;
+    }
+
+    Peak(){
+        return this.BaseArray[0];
+    }
+
+    Clear(){
+        this.BaseArray = [];
+        this.Count = 0;
+    }
+}
